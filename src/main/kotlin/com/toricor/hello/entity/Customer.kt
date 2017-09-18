d38795e9fb664ffe045c8f3166499f98810b635d
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-09-18 16:58:35 +0900

    MyBatis(Kotlin) Customerを題材にsql, Entity, Repositoryを追加

---
+++ src/main/kotlin/com/toricor/hello/entity/Customer.kt
@@ -0,0 +1,8 @@
+package com.toricor.hello.entity
+
+data class Customer(
+        val id: Int,
+        val firstName: String,
+        val lastName: String,
+        val address: String
+)
---
+++ src/main/kotlin/com/toricor/hello/repository/CustomerRepository.kt
@@ -0,0 +1,44 @@
+package com.toricor.hello.repository
+
+import com.toricor.hello.entity.Customer
+import org.apache.ibatis.annotations.Delete
+import org.apache.ibatis.annotations.Insert
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
+import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.SelectKey
+import org.apache.ibatis.annotations.Update
+
+@Mapper
+interface CustomerRepository {
+
+    @Select("SELECT * FROM customers ORDER BY id")
+    fun findAll(): List<Customer>
+
+    @Select("SELECT * FROM customers WHERE id = #{id}")
+    fun findOne(@Param("id") id: Int): Customer
+
+    @Insert("INSERT INTO customers(first_name, last_name, address) VALUES(#{first_name}, #{lastName}, #{address})")
+    @SelectKey(statement = arrayOf("call identity()"), keyProperty = "id", before = false, resultType = Int::class)
+    fun insert(customer: Customer)
+
+    @Update("UPDATE customers SET first_name = #{firstName}, last_name = #{lastName}, address = #{address} WHERE id = #{id}")
+    fun update(customer: Customer)
+
+    @Delete("DELETE FROM customers WHERE id = #{id}")
+    fun delete(@Param("id") id: Int)
+
+    @Select("""
+        SELECT
+            *
+        FROM
+            customers
+        WHERE
+            first_name LIKE '%' || #{firstName} || '%'
+        ORDER BY id
+    """)
+    fun findByFirstName(@Param("firstName") firstName: String): List<Customer>
+
+    @Delete("DELETE FROM customers")
+    fun deleteAll()
+}
