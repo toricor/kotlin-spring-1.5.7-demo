236a0fcea421be9d01c98c8bea3ce5884767d503
imomochi/toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-11-27 19:10:08 +0900

    Update MyApplication.kt

--- src/main/kotlin/com/toricor/hello/MyApplication.kt
+++ src/main/kotlin/com/toricor/hello/MyApplication.kt
@@ -3,6 +3,7 @@ package com.toricor.hello
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+// comment
 @SpringBootApplication
 class HelloApplication
 
